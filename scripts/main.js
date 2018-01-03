0404f2553aee4221325039a1be5268f148da40be
Patrick Koidal <pkoidal519@gmail.com>
2018-01-03 17:56:52 -0600

    update 10
    added twitter follow button using API

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-  function userName() {
+function userName() {
     var userName = prompt("Please enter your name", "<Name goes here>");
 
     if (userName != null) {
     document.getElementById('welcome').innerHTML = "Hello " + userName + "! How are you today?";
   }
 }
-  function createParagraph() {
+function createParagraph() {
     var para = document.getElementById('loremDiv').innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   }
 var myImage = document.querySelector('img');
